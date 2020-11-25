6a26af84c73933af0ebceef25215ba3856c06887
myself jason <j2003nol@gmail.com>
2020-11-25 08:56:56 +0000

    wasd with enter

--- RPG game /main.cpp
+++ RPG game /main.cpp
@@ -26,31 +26,42 @@ int main()
   a.blood =100;
   a.y = 3;
   a.x = 3;
-
+//main game loop
   while(true){
   
-  level1[a.y][a.x] = a.apparence;
-  drawboard1();
+    level1[a.y][a.x] = a.apparence;
+    drawboard1();
 
   //if loop
-  if(level1[a.y-1][a.x] == " ")
-  {
-    level1[a.y][a.x] = " ";
-    a.y--;
-  }
-  else if(level1[a.y][a.x-1] == " ")
-  {
-    level1[a.y][a.x] = " ";
-    a.x--;
-  }
-  int z = 0;
-  cin>>z;
+    char z = ' ';
+    cin>>z;
+    if(z == 'w' && level1[a.y - 1][a.x] != "#")
+    {
+      level1[a.y][a.x] = " ";
+      a.y -=1 ;
+    }
+    else if(z == 's' && level1[a.y + 1][a.x] != "#")
+    {
+      level1[a.y][a.x] = " ";
+      a.y +=1 ;
+    }
+    else if(z == 'd' && level1[a.y][a.x+1] != "#")
+    {
+      level1[a.y][a.x] = " ";
+      a.x +=1 ;
+    }
+    else if(z == 'a' && level1[a.y][a.x-1] != "#")
+    {
+      level1[a.y][a.x] = " ";
+      a.x -=1 ;
+    }
 
-  //break loop
-  if(a.y == 1 && a.x ==0){
-    break;
-  }
-  clean();
+    
+    //break loop
+    if(a.y == 1 && a.x ==0){
+      break;
+    }
+    clean();
   
   }
 
